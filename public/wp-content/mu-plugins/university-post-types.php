d04778259fc2f14e6edc1c806fb3b2352a99433b
Andrew Pacheco <ndrewpacheco@gmail.com>
2024-04-17 23:40:01 -0400

    added custom professor

--- public/wp-content/mu-plugins/university-post-types.php
+++ public/wp-content/mu-plugins/university-post-types.php
@@ -1,23 +1,58 @@
 <?php
-  function university_post_types() {
-    register_post_type( 'event', array(
-      'supports' => array('title', 'editor', 'excerpt'),
-      'rewrite' => array(
-        'slug' => 'events'
-      ),
-      'has_archive' => true,
+function university_post_types()
+{
+  // event post type
+  register_post_type('event', array(
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array(
+      'slug' => 'events'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => "Add New Event",
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+  // program post type
+  register_post_type('program', array(
+    'supports' => array('title', 'editor'),
+    'rewrite' => array(
+      'slug' => 'programs'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => "Add New Programs",
+      'edit_item' => 'Edit Programs',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+    // professor post type
+    register_post_type('professor', array(
+      'supports' => array('title', 'editor'),
       'public' => true,
       'show_in_rest' => true,
       'labels' => array(
-        'name' => 'Events',
-        'add_new_item' => "Add New Event",
-        'edit_item' => 'Edit Event',
-        'all_items' => 'All Events',
-        'singular_name' => 'Event'
+        'name' => 'Professors',
+        'add_new_item' => "Add New Professor",
+        'edit_item' => 'Edit Professor',
+        'all_items' => 'All Professors',
+        'singular_name' => 'Professor'
       ),
-      'menu_icon' => 'dashicons-calendar'
-    ) );
-  }
-  add_action('init', 'university_post_types');
+      'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
+}
 
-?>
+
+
+add_action('init', 'university_post_types');
